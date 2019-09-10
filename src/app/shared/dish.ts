1a9cc5e10d2c0eb394490131ae006a18687bc705
Sony Trinh <sony.trinh@contemi.com.vn>
2019-09-10 10:14:06 +0700

    component part 1

---
+++ src/app/shared/dish.ts
@@ -0,0 +1,10 @@
+export class Dish{
+    id : string;
+    name: string;
+    image: string;
+    label: string;
+    category: string;
+    featured: boolean;
+    price: string;
+    description: string;
+}
